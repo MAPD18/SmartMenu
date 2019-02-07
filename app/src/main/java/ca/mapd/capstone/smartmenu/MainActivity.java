992352414002abefdf1190a5c0a9dbb791aa648d
Rodrigo Vieira <digovs@gmail.com>
2019-02-06 21:00:48 -0500

    Configuring Restaurant and Customer Flavors

--- app/src/main/java/ca/mapd/capstone/smartmenu/MainActivity.java
+++ app/src/main/java/ca/mapd/capstone/smartmenu/MainActivity.java
@@ -3,11 +3,17 @@ package ca.mapd.capstone.smartmenu;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 }
