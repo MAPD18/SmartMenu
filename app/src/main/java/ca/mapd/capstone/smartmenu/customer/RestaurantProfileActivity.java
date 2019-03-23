ba0f174d613b116ff974de0849d71a512b87d7a6
aleixoporpino <aleixoporpino@gmail.com>
2019-03-23 16:44:40 -0400

    Created the restaurant profile files

---
+++ app/src/main/java/ca/mapd/capstone/smartmenu/customer/RestaurantProfileActivity.java
@@ -0,0 +1,15 @@
+package ca.mapd.capstone.smartmenu.customer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.mapd.capstone.smartmenu.R;
+
+public class RestaurantProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_profile);
+    }
+}
