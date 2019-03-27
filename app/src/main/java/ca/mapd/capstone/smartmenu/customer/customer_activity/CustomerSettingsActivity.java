625700bf41c31b825a04ceea6cfb68817baf1518
OguzBayral <oguzbayral93@gmail.com>
2019-03-27 12:14:17 -0400

    Added Bluetooth always on and restaurant broadcast amount settings

--- app/src/main/java/ca/mapd/capstone/smartmenu/customer/customer_activity/CustomerSettingsActivity.java
+++ app/src/main/java/ca/mapd/capstone/smartmenu/customer/customer_activity/CustomerSettingsActivity.java
@@ -160,6 +160,7 @@ public class CustomerSettingsActivity extends AppCompatPreferenceActivity {
     protected boolean isValidFragment(String fragmentName) {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
+                || BluetoothPreferenceFragment.class.getName().equals(fragmentName)
                 || NotificationPreferenceFragment.class.getName().equals(fragmentName);
     }
 
@@ -227,14 +228,15 @@ public class CustomerSettingsActivity extends AppCompatPreferenceActivity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_general);
+            addPreferencesFromResource(R.xml.pref_bluetooth);
             setHasOptionsMenu(true);
 
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
+
+            bindPreferenceSummaryToValue(findPreference("amount_broadcast_restaurant"));
         }
 
         @Override
