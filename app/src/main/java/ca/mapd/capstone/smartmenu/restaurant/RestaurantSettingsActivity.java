f56edb2941b6abd5bf8f092b12e5f66c9e34980d
OguzBayral <oguzbayral93@gmail.com>
2019-04-09 17:33:26 -0400

    Customized the settings for restaurant owners

--- app/src/main/java/ca/mapd/capstone/smartmenu/restaurant/RestaurantSettingsActivity.java
+++ app/src/main/java/ca/mapd/capstone/smartmenu/restaurant/RestaurantSettingsActivity.java
@@ -158,7 +158,7 @@ public class RestaurantSettingsActivity extends PreferenceActivity {
     protected boolean isValidFragment(String fragmentName) {
         return PreferenceFragment.class.getName().equals(fragmentName)
                 || GeneralPreferenceFragment.class.getName().equals(fragmentName)
-                || DataSyncPreferenceFragment.class.getName().equals(fragmentName)
+                || RestaurantBluetoothPreferenceFragment.class.getName().equals(fragmentName)
                 || NotificationPreferenceFragment.class.getName().equals(fragmentName);
     }
 
@@ -222,24 +222,18 @@ public class RestaurantSettingsActivity extends PreferenceActivity {
             return super.onOptionsItemSelected(item);
         }
     }
-
-    /**
-     * This fragment shows data and sync preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class DataSyncPreferenceFragment extends PreferenceFragment {
+    public static class RestaurantBluetoothPreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_data_sync);
+            addPreferencesFromResource(R.xml.pref_bluetooth);
             setHasOptionsMenu(true);
 
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("sync_frequency"));
         }
 
         @Override
@@ -252,4 +246,5 @@ public class RestaurantSettingsActivity extends PreferenceActivity {
             return super.onOptionsItemSelected(item);
         }
     }
+
 }
